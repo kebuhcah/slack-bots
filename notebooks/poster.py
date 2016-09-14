42107199ae15b679040a69fa904f1498a350413f
Kevin Sun <kevsun1@gmail.com>
2016-09-14 07:51:23 -0400

    fix secondsPerPost logic & avoid div-by-0 error

--- notebooks/poster.py
+++ notebooks/poster.py
@@ -52,12 +52,14 @@ user_emoji_map = {
     'Devin' : ':alien:',
 }
 
-for i in range(0,len(content)):
+for i in range(40,len(content)):
     user = content.ix[i]['user']
     message = content.ix[i]['message']
-    remainingPosts = len(content) - i
-    remainingSeconds = (datetime.datetime(2016, 9, 12, 17) - datetime.datetime.now()).total_seconds()
-    secondsPerPost = remainingSeconds / remainingPosts
+    secondsPerPost = 0
+    if len(content) != i and True:
+        remainingPosts = len(content) - i - 1
+        remainingSeconds = (datetime.datetime(2016, 9, 14, 7) - datetime.datetime.now()).total_seconds()
+        secondsPerPost = remainingSeconds / remainingPosts
     print i,
     try:
         slackPost(message, 'Markov ' + user, user_emoji_map[user], '#markov')
